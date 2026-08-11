10fd89ed942e40962a6b7c625ec640ac91da711c
Mahamba-66 <mahamba@esp.sngit config --global user.name>
2026-08-11 03:50:26 +0200

    schema database

--- server/prisma.config.ts
+++ server/prisma.config.ts
@@ -3,7 +3,6 @@ import { env } from "node:process";
 import { defineConfig } from "prisma/config";
 
 const databaseUrl = env.DATABASE_URL;
-
 if (!databaseUrl) {
   throw new Error("DATABASE_URL is not defined in the environment variables.");
 }
